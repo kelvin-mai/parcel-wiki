55aa4dd96dd597d0a66b82502d31c33698c5c27e
Kelvin Moi <kelvin.mai002@gmail.com>
2018-04-01 22:29:56 -0500

    Completed functionality

---
+++ README.md
@@ -0,0 +1,12 @@
+# Wikipedia Viewer
+
+freeCodeCamp Build a Wikipedia Viewer
+
+## User Stories
+
+* I can search Wikipedia entries in a search box and see the resulting Wikipedia entries.
+* I can click a button to see a random Wikipedia entry.
+
+## Built with
+
+* [parceljs](https://parceljs.org/getting_started.html) - Blazing fast, zero configuration web application bundler
--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('test');
+const API =
+	'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
+const PAGE = 'https://en.wikipedia.org/?curid=';
+
+const search = term => {
+	fetchJsonp(`${API}&gsrsearch=${term}`)
+		.then(res => res.json())
+		.then(data => displaySearch(data.query.pages))
+		.catch(err => console.log(err));
+};
+
+const displaySearch = obj => {
+	document.getElementById('results').innerHTML = '';
+	for (i in obj) {
+		displayResult(obj[i]);
+	}
+};
+
+const displayResult = obj => {
+	document.getElementById('results').innerHTML += `
+		<li>
+			<div>
+				<h3>${obj.title}</h3>
+				<p>${obj.extract}</p>
+				<a href='${PAGE}${obj.pageid}'>Read More</a>
+			</div>
+		</li>
+	`;
+};
+
+(global => {
+	document.getElementById('form').addEventListener('submit', e => {
+		e.preventDefault();
+		search(e.target[0].value);
+	});
+})(window);
